f35e4f3c3e74d4dd794aeb018cdcb10ba686de18
myusername <rodriguez.jamie@live.com>
2019-08-05 16:43:48 -0400

    replaced env file

--- outboundSMS.js
+++ outboundSMS.js
@@ -1,7 +1,9 @@
+//Required variables
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
+//Sending outbound message
 client.messages
   .create({
      body: 'Hello, Welcome to my Twilio account',
